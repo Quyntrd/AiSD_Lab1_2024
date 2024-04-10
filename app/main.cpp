a283cf4ef4e65a9897303e0e2958ea7d314cf922
Quyntrd <daniil.terebilkib@gmail.com@git config --global user.name Quyntrdgit config --global user.email daniil.terebilkib@gmail.com@>
2024-04-10 18:38:37 +0400

    added function in main.cpp and main body

---
+++ app/main.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+
+#include "BST.cpp"
+
+using namespace std;
+
+vector<int> find_uniques(const vector<int>& in) {
+	vector<int> result;
+	BinarySearchTree<int> bst;
+	for (int i : in) {
+		bool flag = bst.insert(i);
+		if (flag) { // Ёквивалентность if(flag) и if(flag == true)
+			result.push_back(i);
+		}
+		else {
+			result.pop_back();
+		}
+	}
+	return result;
+}
+int main() {
+	vector<int> vect{1, 5, 5, 2, 7, 9, 11, 11};
+	vector<int> unique = find_uniques(vect);
+	cout << "first vector: ";
+	for (int i = 0; i < vect.size(); ++i) {
+		cout << vect[i] << " ";
+	}
+	cout << endl;
+	cout << "unique vector: ";
+	for (int i = 0; i < unique.size(); ++i) {
+		cout << unique[i] << " ";
+	}
+	
+}
--- src/BST.cpp
+++ src/BST.cpp
@@ -1,9 +1,42 @@
-﻿#include "Set.h"
+﻿#pragma once
+#include <random>
+#include <stdexcept>
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class BinarySearchTree {
+	struct Node {
+		T data;
+		Node* left;
+		Node* right;
+		Node(const T d) : data(d), left(nullptr), right(nullptr) {};
+	};
+	Node* root;
+	Node* copy_tree(const Node* other);
+	Node* find_min(Node* node) const;
+	Node* search_recursive(Node* node, T key);
+	void erase_recursive(Node*& node, const T key);
+	void destroy_recursive(Node* node);
+	void print_recursive(Node* node, const string& prefix = "", bool isLeft = true) const;
+	bool contains_recursive(Node* node, const T key) const;
+	int count_recursive(Node* node);
+public:
+	BinarySearchTree() : root(nullptr) {};
+	BinarySearchTree(const BinarySearchTree& other);
+	~BinarySearchTree();
+	BinarySearchTree& operator=(const BinarySearchTree& other);
+
+	void print() const;
+	bool insert(T key);
+	bool contains(T key) const;
+	bool erase(T key);
+	int count();
+	bool search(T key);
+};
 
 template<typename T>
-BinarySearchTree<T>::BinarySearchTree(): root(nullptr){}
-template<typename T>
-BinarySearchTree<T>::Node* BinarySearchTree<T>::copy_tree(const Node* other) {
+typename BinarySearchTree<T>::Node * BinarySearchTree<T>::copy_tree(const Node* other) {
 	if (!other) {
 		return nullptr;
 	}
@@ -53,11 +86,11 @@ void BinarySearchTree<T>::print() const {
 template<typename T>
 bool BinarySearchTree<T>::insert(T key) {
 	if (!root) {
-		root = new Node(T);
+		root = new Node(key);
 		return true;
 	}
 	Node* tmp = root;
-	while (tmp && tmp->data != key) {
+	while (tmp) {
 		if (tmp->data > key) {
 			if (tmp->left)
 				tmp = tmp->left;
@@ -120,7 +153,7 @@ void BinarySearchTree<T>::erase_recursive(Node*& node, const T key) {
 	}
 }
 template<typename T>
-BinarySearchTree<T>::Node* BinarySearchTree<T>::find_min(Node* node) const {
+typename BinarySearchTree<T>::Node * BinarySearchTree<T>::find_min(Node* node) const {
 	while (node->left) {
 		node = node->left;
 	}
@@ -144,7 +177,7 @@ int BinarySearchTree<T>::count() {
 	return count_recursive(root);
 }
 template<typename T>
-BinarySearchTree<T>::Node* BinarySearchTree<T>::search_recursive(Node* node, T key) {
+typename BinarySearchTree<T>::Node * BinarySearchTree<T>::search_recursive(Node* node, T key) {
 	if (!node || node->data == key) return root;
 	if (node->data < key)
 		return search_recursive(node->right, key);
