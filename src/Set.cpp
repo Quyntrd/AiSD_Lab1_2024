fa0cbd7f9c090ef594ba34cd8a8cd5077edd8d1c
Quyntrd <daniil.terebilkib@gmail.com@git config --global user.name Quyntrdgit config --global user.email daniil.terebilkib@gmail.com@>
2024-04-10 13:15:07 +0400

    added CMakeLists.txt Set.h Set.cpp main.cpp

---
+++ src/Set.cpp
@@ -0,0 +1,12 @@
+﻿// AiSD_lab1_2024.cpp: определяет точку входа для приложения.
+//
+
+#include "AiSD_lab1_2024.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
